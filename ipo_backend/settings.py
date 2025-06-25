eeb8eed7c6d1ba88061e97666b2765a2797d3d51
khushi <sharmakhushi97835@gmail.com>
2025-06-25 12:29:06 +0530

    fix: Add CORS support

--- ipo_backend/settings.py
+++ ipo_backend/settings.py
@@ -74,6 +74,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ipo_backend.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://enchanting-sprinkles-296986.netlify.app"
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
